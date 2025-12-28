310b03212ce01b7187d40770d29570248b95ef2e
Kanchana Kalansooriya <92777840+Kanchanamkl@users.noreply.github.com>
2025-12-28 14:48:25 +0530

    fixed Policy Violation: Keyword Stuffing

--- next.config.js
+++ next.config.js
@@ -24,7 +24,6 @@ const nextConfig = {
   },
   env: {
     NEXT_PUBLIC_ONEPAY_APP_ID: process.env.ONEPAY_APP_ID,
-    NEXT_PUBLIC_ONEPAY_APP_TOKEN: process.env.ONEPAY_APP_TOKEN,
     NEXT_PUBLIC_ONEPAY_CURRENCY: process.env.ONEPAY_CURRENCY,
     NEXT_PUBLIC_APP_URL: process.env.NEXT_PUBLIC_APP_URL || 'http://localhost:3000',
   },
